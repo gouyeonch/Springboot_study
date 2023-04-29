dec2965e2347423615d42f5077ab006ad4d680f8
gouyeonch <gouyeonch@naver.com>
2023-04-29 22:59:07 +0900

    1. 개발환경 만들기

---
+++ myfisrtproject/src/main/java/com/example/myfisrtproject/MyfisrtprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfisrtproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfisrtprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfisrtprojectApplication.class, args);
+	}
+
+}

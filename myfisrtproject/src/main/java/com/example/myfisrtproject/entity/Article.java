085bcfa6fea42d1a9d55fb0f4972bbddf503c51c
gouyeonch <gouyeonch@naver.com>
2023-04-30 20:26:30 +0900

    11. 데이터 조회하기 with JPA

--- myfisrtproject/src/main/java/com/example/myfisrtproject/entity/Article.java
+++ myfisrtproject/src/main/java/com/example/myfisrtproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.myfisrtproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -8,7 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@AllArgsConstructor
+@AllArgsConstructor // 모든 멤버값을 초기화해주는 생성자 추가
+@NoArgsConstructor // 디폴트 생성자 추가
 @ToString
 @Entity // DB가 해당 객체를 인식 가능
 public class Article {
